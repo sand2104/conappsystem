3bc41b5e76b3dc8e20ab187630cfa92cccb898fa
sand2104 <prasadsandy2000@gmail.com>
2023-07-11 12:34:17 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conappsystem
+{
+    internal class Program
+    {
+        static void Main(string[]args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 7)
+                {
+                    goto end;
+                }
+                Console.WriteLine("i value: {0}", i);
+            }
+        end: Console.WriteLine("The end");
+        }
+    }
+}
+
